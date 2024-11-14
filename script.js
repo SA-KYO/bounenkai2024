b8ba7374b637a11a72241c960eaf2cbec1f3b775
SA-KYO <78464699+SA-KYO@users.noreply.github.com>
2024-11-14 09:26:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,3 @@ ScrollSmoother.create({
 });
 
 
-// 💚 This just adds the GSAP link to this pen, don't copy this bit
-import { GSAPInfoBar } from "https://codepen.io/GreenSock/pen/vYqpyLg.js"
-new GSAPInfoBar({ link: "https://gsap.com/docs/v3/Plugins/ScrollSmoother/"});
-// 💚 Happy tweening!
